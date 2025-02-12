1c4fbf3983171d8d2ae7a0bd8991e2db62513468
suelenbraga1 <eusuelenbraga@gmail.com>
2025-02-12 10:50:46 -0300

    finalizando tela de login e iniciando a tela de reembolso

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login.jsx"
+import Solicitacao from "./components/solicitacao/Solicitacao.jsx";
+import "./global.scss"
+import Reembolsos from "./components/reembolsos/Reembolsos.jsx";
 
-function App() {
+function App(){
   return(
-    <>
-     <Login />
-    </>
-  )
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Login/>}/>
+    <Route path="/reembolsos" element={<Reembolsos/>}/>
+    <Route path="/solicitacao" element={<Solicitacao/>}/>
+  </Routes>
+</BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,34 +1,47 @@
-import Logo from "../../assets/Tela Login/logo-ws.png"
-import Capa from "../../assets/Tela Login/imagem tela de login.png"
-
+import { useNavigate} from "react-router-dom";
+import Logo from "../../assets/Tela Login/logo-ws.png";
+import Capa from "../../assets/Tela Login/imagem tela de login.png";
+import styles from "./Login.module.scss";
 
 
 function Login(){
+
+    const navigate = useNavigate() //iniciando o hook useNavigate
+
+    const irParaReembolsos = () => {
+        navigate("/reembolsos")  //redirecionando pra a pagina de reembolsos
+        }
+
+
+
+
+
+
+
     return(
-        <main>
-            <section> </section>
+        <main className={styles.container}>
+            
+            <img src={Capa} alt="Foto de um navio cargueiro"/>
+            
 
-            <section>
+            <section className={styles.login}>
                 <img src={Logo} alt="Logo da Wilson Sons"/>
-
-                <h1>Boas vindas ao Novo portal SISPAR</h1>
-
+                <h1>Boas vindas ao Novo Portal SISPAR</h1>
                 <p>Sistema de Emissão de Boletos e Parcelamentos </p>
 
-                <form action="">
+                <form>
                     <input type="email" name="email" id="email" placeholder="Email"/>
                     <input type="password" name="password" id="password" placeholder="Senha"/>
-                    <a href="">Esqueci minha senha</a>
-                    
+                    <a href="#">Esqueci minha senha</a>
+
+                    <div className={styles.buttons}>
+                    <button onClick={irParaReembolsos} className={styles.btnLogin}>Entrar</button>
+                    <button className={styles.btnCriar}>Criar conta</button>
+                    </div>
                 </form>
-                
-                <div>
-                    <button>Entrar</button>
-                    <button>Criar conta</button>
-                </div>
-                 </section>
+            </section>
         </main>
-    )
+    );
 }
 
-export default Login
+export default Login;
